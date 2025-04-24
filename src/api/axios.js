3b9310bf41a192aad8633e0d2c0bc26609a98904
SzalokiKlaudia <szaloki.klaudia90@gmail.com>
2025-04-24 17:27:31 +0200

    config

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const  myAxios=axios.create({
 
-    baseURL: process.env.REACT_APP_API_URL || '/api',
+    baseURL: process.env.REACT_APP_API_URL || '/',
 
 
     withCredentials: true, //kérések atzonosítása cookie-k segítségével
