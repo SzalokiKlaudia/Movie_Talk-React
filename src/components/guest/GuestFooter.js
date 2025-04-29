14846d7e62c6d3cbd9cabb5c17dc07126319ad3f
SzalokiKlaudia <szaloki.klaudia90@gmail.com>
2025-04-29 18:05:45 +0200

    logo

--- src/components/guest/GuestFooter.js
+++ src/components/guest/GuestFooter.js
@@ -3,10 +3,11 @@ import React from 'react'
 import  '../../style/Footer.css';
 
 export default function GuestFooter() {
+  const defaultLogo = 'http://localhost:3000/images/tmdb.svg'
   return (
     <div className='footer bg-dark h-20'> 
      <a href="https://www.themoviedb.org" target="_blank" rel="noopener noreferrer">
-            <img src="/images/tmdb.svg" alt="TMDb Logo" className="logo-footer"/>
+            <img src={defaultLogo} alt="TMDb Logo" className="logo-footer"/>
         </a>
 
     </div>
