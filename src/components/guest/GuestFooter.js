659e1e238026a7328e50ec196ccc1f7ae476d5d8
SzalokiKlaudia <szaloki.klaudia90@gmail.com>
2025-04-29 18:08:45 +0200

    logo fix

--- src/components/guest/GuestFooter.js
+++ src/components/guest/GuestFooter.js
@@ -3,7 +3,7 @@ import React from 'react'
 import  '../../style/Footer.css';
 
 export default function GuestFooter() {
-  const defaultLogo = 'http://localhost:3000/images/tmdb.svg'
+  const defaultLogo = 'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg'
   return (
     <div className='footer bg-dark h-20'> 
      <a href="https://www.themoviedb.org" target="_blank" rel="noopener noreferrer">
